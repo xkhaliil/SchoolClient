ec747f43da9b4053243701a67f5da55b551ad701
wassim.beltaief <beltaief.wassim@gmail.com>
2023-02-09 17:10:19 +0100

    edit missing

--- backend/main.js
+++ backend/main.js
@@ -12,7 +12,7 @@ app.use(bodyparser.json())
 
 app.get('/', (res) => {
     res.status(200).send("Hello")
-}
+})
 
 app.post('/firebase/notification', (req, res) => {
     //const message = req.body.message
